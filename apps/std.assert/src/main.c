b2bb894993b77012c7cba540dd2aebfa2b5a8b55
Peng Li <aqnote@qq.com>
2019-04-12 14:25:37 +0800

    remove static_assert from c code

--- apps/std.assert/src/main.c
+++ apps/std.assert/src/main.c
@@ -36,11 +36,12 @@ int test_assert() {
  * 编译时判定表达式是否正确
  */
 int test_static_assert() {
+#ifdef __cplusplus
     static_assert(1.0 > 0.0, "1.0 <= 0.0");
     static_assert(1 + 2 == 3, "1 + 2 <> 3");
     static_assert(sizeof(int) > sizeof(char), "sizeof int is more than char");
     static_assert(1, "1 is TRUE");
-
+#endif
     return 0;
 }
 
